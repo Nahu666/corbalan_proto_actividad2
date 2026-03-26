b99007b5a38f17da442c065e0e3e6a55c7128e5b
Nahu <nahucorbalan06@gmail.com>
2026-03-26 16:24:33 -0300

    Crear al Enemigo

--- proto-actividad-2/player.gd
+++ proto-actividad-2/player.gd
@@ -3,19 +3,27 @@ extends Area2D
 @export var speed = 400
 var screen_size
 
-
+signal hit
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
+func _on_body_entered(_body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
 
-
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-		print(velocity)
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_down"):
